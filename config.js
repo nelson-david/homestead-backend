c56e5013c8fd7c2db1b4f039794ab30d8892c88a
nelson-david <larva.code15@gmail.com>
2021-08-06 00:07:00 +0100

    changed connection string

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
 		origin: "http://localhost:3000",
 		buildOrigin: "http://localhost:7799",
 		devOrigin: "http://192.168.43.191:3000",
-		atlasURL: "mongodb+srv://nelson:akelachi8899@giftcluster.aaori.mongodb.net/giftapp?retryWrites=true&w=majority",
+		atlasURL: "mongodb+srv://DavidNelson:xCrNyznFBvl3fnM1@homestead-db.gjiqg.mongodb.net/homesteadapi-db?retryWrites=true&w=majority",
 		secDevOrigin: "http://192.168.43.192:3000",
 		mongoURL: "mongodb://localhost:27017/homeStead",
 		TOKEN_SECRET: 'dkngfngngg'
