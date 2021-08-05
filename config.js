f698f53a356509c1f65d373767db92d7b81d14a9
nelson-david <larva.code15@gmail.com>
2021-08-06 00:53:46 +0100

    Added New Cors Origin

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
 	development: {
 		port: process.env.PORT || 7500,
 		origin: "http://localhost:3000",
-		buildOrigin: "http://localhost:7799",
+		buildOrigin: "https://brave-hopper-6e87df.netlify.app",
 		devOrigin: "http://192.168.43.191:3000",
 		atlasURL: "mongodb+srv://DavidNelson:xCrNyznFBvl3fnM1@homestead-db.gjiqg.mongodb.net/homesteadapi-db?retryWrites=true&w=majority",
 		secDevOrigin: "http://192.168.43.192:3000",
